eaee6077e41bf4d183a5d81582199e906a9915b0
kevin930620 <kevin0974004568@gmail.com>
2024-11-09 21:46:41 +0800

    start

---
+++ web/app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+app = Flask(__name__)
+@app.route("/")
+def hello():
+    return "Hello, World!"
+
+@app.route("/admin")
+def admin():
+    # return admin html page
+    return ""
